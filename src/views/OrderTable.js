d25c34430f43388931416b2c5c7f8d1aed5c0625
Nuvindu <nuvidu.18@cse.mrt.ac.lk>
2023-06-01 21:38:24 +0530

    Apply changes to display values of sse requests in the table

--- src/views/OrderTable.js
+++ src/views/OrderTable.js
@@ -17,38 +17,23 @@ function createData(source, date, time, traffic) {
 
 function DataTable() {
 
-  // const [data, setData] = useState([])
+  const [rows, setRows] = useState([]);
+  const [allRows, setAllRows] = useState([]);
 
-  // useEffect(() => {
-  //   DataService.postData()
-  //   const sse = new EventSource('http://localhost:8081/sse');
-  //   let ls = []
-
-
-  //   sse.onmessage = e => getRealtimeData(JSON.parse(e.data));
-
-  //   return () => {
-  //     sse.close();
-  //   };
-  // }, []);
-  const [data, setData] = useState([]);
-  let eventSource;
   useEffect(() => {
-
     let isMounted = true;
 
     function getRealtimeData(data) {
-      let ls = []
+      let ls = [];
       data.forEach(element => {
-        ls.push(JSON.parse(element))
+        ls.push(JSON.parse(element));
       });
       return ls;
     }
 
     const fetchData = async () => {
-      
       await DataService.postData();
-      eventSource = await fetchEventSource(`${serverBaseURL}/orderInfo?userId=QWERTY`, {
+      const eventSource = await fetchEventSource(`${serverBaseURL}/orderInfo?userId=QWERTY`, {
         method: "GET",
         headers: {
           Accept: "text/event-stream",
@@ -65,13 +50,14 @@ function DataTable() {
           }
         },
         onmessage(event) {
-          console.log(event.data);
           const parsedData = JSON.parse(event.data);
-          // const finalData = getRealtimeData(parsedData);
-          console.log(parsedData);
 
           if (isMounted) {
-            setData(parsedData);
+            setRows(parsedData);
+            setAllRows(prevRows => {
+              const newRows = [...prevRows, parsedData];
+              return newRows.length > 20 ? newRows.slice(newRows.length - 20) : newRows;
+            });
           }
         },
         onclose() {
@@ -81,54 +67,43 @@ function DataTable() {
           console.log("There was an error from server", err);
         },
       });
+
+      return eventSource;
     };
-    fetchData();
+
+    const eventSource = fetchData();
 
     return () => {
-      // 👇️ when component unmounts, set isMounted to false
       isMounted = false;
-      eventSource.close();
+      eventSource.then(source => source.close());
     };
   }, []);
-  
 
-  const rows = data;
-  // console.log(data)
-  // const rows = [
-  //   createData(data[0].ip, data[0].date, data[0].time, data[0].traffic),
-  //   createData(data[1].ip, data[1].date, data[1].time, data[1].traffic),
-  //   createData(data[2].ip, data[2].date, data[2].time, data[2].traffic),
-  //   createData(data[3].ip, data[3].date, data[3].time, data[3].traffic),
-  //   createData(data[4].ip, data[4].date, data[4].time, data[4].traffic),
-  // ];
+  // console.log("Data: ", rows);
+  console.log("All Data: ", allRows);
+
   return (
     <TableContainer sx={{ color: '#595959' }} component={Paper} backgroundcolor='#595959' padding={'10'} >
       <Table sx={{ minWidth: 650 }} aria-label="simple table" variant='simple' >
-        <TableHead> 
+        <TableHead>
           <TableRow>
             <TableCell>Order ID</TableCell>
             <TableCell align="right">Item Type</TableCell>
-            <TableCell align="right">Unit Price</TableCell>   
+            <TableCell align="right">Unit Price</TableCell>
             <TableCell align="right"><Tooltip title="Total Cost"><div>Total Revenue</div></Tooltip></TableCell>
-            <TableCell align="right"><Tooltip title="Total Cost"><div>Total Cost</div></Tooltip></TableCell>        
+            <TableCell align="right"><Tooltip title="Total Cost"><div>Total Cost</div></Tooltip></TableCell>
             <TableCell align="right"><Tooltip title="Total Cost"><div>Total Profit</div></Tooltip></TableCell>
           </TableRow>
         </TableHead>
-
         <TableBody>
-
-          {rows.slice(0).reverse().map((row, i) => (
-            
-            <TableRow
-              key={i}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">{row[0]}</TableCell>
-              <TableCell align="right" >{row[1]}</TableCell>
+          {allRows.reverse().map((row, index) => (
+            <TableRow key={index} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+              <TableCell align="right">{row[1]}</TableCell>
               <TableCell align="right">{row[2]}$</TableCell>
               <TableCell align="right">{row[3]}$</TableCell>
               <TableCell align="right">{row[4]}$</TableCell>
               <TableCell align="right">{row[5]}$</TableCell>
+              <TableCell component="th" scope="row">{row[6]}</TableCell>
             </TableRow>
           ))}
         </TableBody>
